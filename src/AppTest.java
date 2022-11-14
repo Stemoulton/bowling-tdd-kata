d0b0a2faabdb0b97fa9afd38d2bab4b2173168ba
Stephen M <stephen.m@raytheon.co.uk>
2022-11-14 13:26:05 +0000

    First 2 tests

--- src/AppTest.java
+++ src/AppTest.java
@@ -1,8 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
-    // @Test
-    // public void testMain() {
 
-    // }
+    @Test
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++) {
+            g.roll(0);
+        }
+
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
+
 }
