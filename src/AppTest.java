80807c5f823211477a210b10d93b26c4af48ee4a
Stephen M <stephen.m@raytheon.co.uk>
2022-11-14 13:40:27 +0000

    Spare added

--- src/AppTest.java
+++ src/AppTest.java
@@ -4,22 +4,38 @@ import org.junit.Test;
 
 public class AppTest {
 
+    private Game g = new Game();
+
+    public void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
+    }
+
     @Test
     public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i = 0; i < 20; i++) {
-            g.roll(0);
-        }
-
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
 
     @Test
     public void testAllOnes() throws Exception {
-        Game g = new Game();
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20, 1);
         assertEquals(20, g.score());
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+
+    @Test
+    public void testOneSpare() throws Exception {
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+
+        assertEquals(16, g.score());
     }
 
 }
